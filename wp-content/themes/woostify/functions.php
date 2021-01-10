20bad58f8a763748eb4281d89f2545207c15a451
GaneshD076 <67255147+GaneshD076@users.noreply.github.com>
2021-01-10 22:03:08 +0530

    New task
    New task

--- wp-content/themes/woostify/functions.php
+++ wp-content/themes/woostify/functions.php
@@ -70,3 +70,80 @@ function myshortcode_posts( $att ) {
 	   return $return_string;
 }
 add_shortcode( 'myshortcode', 'myshortcode_posts' );
+add_action( 'woocommerce_new_order', 'create_invoice_for_wc_order',  1, 1  );
+function create_invoice_for_wc_order( $order_id ) {
+   include_once('api.php');
+$order_data=array();
+$billingAddress=array();
+$shippingAddress=array();
+$items=array();
+$query = new WC_Order_Query( array(
+      'p'         => $order_id, 
+		'post_type' => 'shop_order'
+) );
+$orders = $query->get_orders();
+ foreach ($orders as $or) {
+$order_data['id']=$or->id;
+$order_data['customerOrderNumber']=$or->id;
+$order = wc_get_order( $order_data['id']);
+    foreach( $order->get_items( 'shipping' ) as $item_id => $item ){
+    $order_data['shipMethod']= $item->get_method_title();
+    }
+$data=$or->data;
+$billing=$data['billing'];
+$shipping=$data['shipping'];
+
+$payment_method=$data['payment_method'];
+if($payment_method=="cod"){$order_data['isCOD']="1";}else{$order_data['isCOD']="0";}
+$date_created=$data['date_created'];
+$order_data['orderDate']=get_the_date( 'Y-m-d H:i:s', $or->id);;
+$order_data['paymentType']=$payment_method;
+$order_data['taxAmount']=$data['total_tax'];
+$order_data['shipChargeAmount']=$data['shipping_total'];
+$order_data['orderTotal']=$data['total'];
+$fname=$billing['first_name'];
+$lname=$billing['last_name'];
+$billingAddress['customerName']=$fname.' '.$lname;
+$billingAddress['addressLine1']=$billing['address_1'];
+$billingAddress['addressLine2']=$billing['address_2'];
+$billingAddress['city']=$billing['city'];
+$billingAddress['state']=$billing['state'];
+$billingAddress['postalCode']=$billing['postcode'];
+$billingAddress['countryName']=$billing['country'];
+$billingAddress['email']=$billing['email'];
+$billingAddress['contactPhone']=$billing['phone'];
+$order_data['billingAddress']=$billingAddress;
+$fname=$shipping['first_name'];
+$lname=$shipping['last_name'];
+$shippingAddress['customerName']=$fname.' '.$lname;
+$shippingAddress['addressLine1']=$shipping['address_1'];
+$shippingAddress['addressLine2']=$shipping['address_2'];
+$shippingAddress['city']=$shipping['city'];
+$shippingAddress['state']=$shipping['state'];
+$shippingAddress['postalCode']=$shipping['postcode'];
+$shippingAddress['countryName']=$shipping['country'];
+$order_data['shippingAddress']=$shippingAddress;
+$i=1;
+foreach ($order->get_items() as $item_key => $item ):
+   $product      = $item->get_product();
+   $item_data    = $item->get_data();
+$items['lineItemSequenceNumber']=$i;
+$items['itemID']=$product->id;
+$items['productName']=$item_data['name'];
+$items['quantity']=$item_data['quantity'];
+$productprice = new WC_Product($product->id);
+$items['customerPrice']=$product->get_price();
+$items['subtotal']=$item_data['subtotal'];
+$items['lineItemTotal']=$item_data['total'];
+$items['taxAmount']=$item_data['total_tax'];
+$image=wp_get_attachment_image_src(get_post_thumbnail_id($product->id),'single-post-thumbnail');
+$items['productUrl']=get_permalink( $product->id );
+$items['productImageUrl']=$image[0];
+$items['sku']=$product->get_sku();
+$order_data['items'][]=$items;
+$i++;
+endforeach;
+}
+$endpoint="http://localhost/out/awdiz/api/";
+callAPI('POST', $endpoint, json_encode($order_data), $headers );
+}
